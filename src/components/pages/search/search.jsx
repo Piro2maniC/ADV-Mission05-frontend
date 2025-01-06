2c0c599342ff56ca882b157a3fa7e3cf9b8725c5
David Tolmay <davidt@missionreadyhq.com>
2025-01-06 13:13:48 +1300

    made case changes to file names

--- src/components/pages/search/search.jsx
+++ src/components/pages/search/search.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import searchStyles from "../../styles/Search.module.css";
+import globalStyles from "../../styles/Global.module.css";
 
 function search() {
   return (
     <div>
+      <div className= {searchStyles.container} > </div>
+      <div className= {globalStyles.header} > </div>
+
       
     </div>
   )
