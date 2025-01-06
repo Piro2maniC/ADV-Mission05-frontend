d717b5bd4686bab6518b0712660a31bfb06355c9
Rosanna Moss <rosannam@missionreadyhq.com>
2025-01-06 14:23:21 +1300

    app changed to show my header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Items from './components/Items';
 import styles from './components/styles.module.css';
+import Header from './components/sharedComponents/searchBarHeader/ConstHeader.jsx'
 
 const App = () => {
     return (
         <Router>
             <div className={styles.app}>
-                <Routes>
+                {/* <Routes>
                     <Route path="/" element={<Items />} />
-                </Routes>
+                </Routes> */}
+                <Header></Header>
             </div>
         </Router>
     );
