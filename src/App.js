324afd449770f4c18349aaa89d70d3eb0c3c013d
Rosanna Moss <rosannam@missionreadyhq.com>
2025-01-08 15:08:35 +1300

    commented out app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Items from './components/Items';
-import styles from './components/styles.module.css';
-import Header from './components/pages/landing/LandingHeader.jsx';
-import Landing from './components/pages/landing/Landing.jsx'
+// // src/App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import Items from './components/Items';
+// import styles from './components/styles.module.css';
+// import Header from './components/pages/landing/LandingHeader.jsx';
+// import Landing from './components/pages/landing/Landing.jsx'
 
-const App = () => {
-    return (
-        // <Router>
-            <div className={styles.app}>
-                {/* <Routes>
-                    <Route path="/" element={<Items />} />
-                </Routes> */}
-                <Header></Header>
-                <Landing></Landing>
-            </div>
-        //{/* </Router> */}
-    );
-};
+// const App = () => {
+//     return (
+//         // <Router>
+//             <div className={styles.app}>
+//                 {/* <Routes>
+//                     <Route path="/" element={<Items />} />
+//                 </Routes> */}
+//                 <Header></Header>
+//                 <Landing></Landing>
+//             </div>
+//         //{/* </Router> */}
+//     );
+// };
 
-export default App;
+// export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LayoutLanding from "./common/Layout-Landing/LayoutLanding.jsx";
-import Landing from "./components/pages/landing/Landing.jsx";
-import Search from "./components/pages/search/search.jsx";
-import Compare from "./components/pages/compare/compare.jsx";
+import Landing from "./components/pages/Landing/Landing.jsx";
+import Search from "./components/pages/Search/Search.jsx";
+import Compare from "./components/pages/Compare/Compare.jsx";
+import Listing from "./components/pages/listing/listing.jsx";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/search" element={<Search />} />
           <Route path="/compare" element={<Compare />} />
+          <Route path="/listing/:id" element={<Listing id={true} />} />
         </Route>
       </Routes>
     </div>
